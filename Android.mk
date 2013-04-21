575c0ef3a8d5ddbe8b0276ff2e42d69ac13be711
AndroidMeda <hephappy@gmail.com>
2013-04-21 21:45:57 +0300

    test
    Change-Id: I5d8be0140c1544c656c3c94e8da3f7137dfa3cc7

--- Android.mk
+++ Android.mk
@@ -25,7 +25,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-fil
 
 LOCAL_PACKAGE_NAME := Trebuchet
 LOCAL_CERTIFICATE := shared
-
 LOCAL_OVERRIDES_PACKAGES := Home Launcher2
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
